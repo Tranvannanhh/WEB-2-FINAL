97400dc3845f7e7e5e97b3df1a9dad2bf231d466
Tranvannanhh <22070395@vnu.edu.vn>
2026-06-21 21:10:18 +0700

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,8 +7,6 @@ require_once __DIR__ . '/../models/User.php';
 require_once __DIR__ . '/../models/Facility.php';
 require_once __DIR__ . '/../models/Report.php';
 
-requireAdmin();
-
 $action = $_GET['action'] ?? $_POST['action'] ?? '';
 
 switch ($action) {
